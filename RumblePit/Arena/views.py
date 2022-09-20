8a506079fd90c2d8ace7a433bc38bafacc06fb86
WMorf <wesmorford@outlook.com>
2022-09-20 13:58:38 -0700

    Created Model and model manager

--- RumblePit/Arena/views.py
+++ RumblePit/Arena/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render, HttpResponse
 
 # Arena Home Page
 def arena_home(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'Arena/arena_home.html')
